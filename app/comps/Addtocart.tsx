269a2a882d3c7637ba57390a0ab4af27c14dd5fa
IliseiRares <iliseiraresflorin@gmail.com>
2024-04-09 22:20:14 +0300

    Commit Sr-next

---
+++ app/comps/Addtocart.tsx
@@ -0,0 +1,22 @@
+'use client' ;
+
+import React, { useState } from 'react';
+
+
+const AddToCart = () => {
+  const [isAdded, setIsAdded] = useState(false);
+
+  const handleAddToCart = () => {
+    setIsAdded(true);
+  };
+
+  return (
+    <div>
+      <button onClick={handleAddToCart}>
+        {isAdded ? 'Added to cart' : 'Add to cart'}
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
---
+++ app/comps/carcard.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import Addtocart from './Addtocart';
+const Carcard = () => {
+  return (
+    <div>
+      <Addtocart />
+    </div>
+  )
+}
+
+export default Carcard
